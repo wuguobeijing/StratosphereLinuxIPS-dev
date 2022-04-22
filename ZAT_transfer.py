8cf7cbd09ca6bbe6f5eea4a914b7effabbd358b4
wuguobeijing <wuguobeijing@q163.com>
2022-04-22 20:04:50 +0800

    4-22

--- ZAT_transfer.py
+++ ZAT_transfer.py
@@ -96,11 +96,15 @@ if __name__ == '__main__':
         if topic not in exist_topic:
             json_content = {"type": topic}
             producer.send('Topic_list', json_content)
-            topic_list.append(NewTopic(name=topic, num_partitions=1, replication_factor=1))
-        #     admin_client.create_topics(new_topics=topic_list, validate_only=True)
+            # topic_list.append(NewTopic(name=topic, num_partitions=1, replication_factor=1))
+            # admin_client.create_topics(new_topics=topic_list, validate_only=True)
         # zeek_df = extract_log('zeek_files/'+file)
         # write_to_producer(producer, zeek_df, topic=topic)
+        f = open('zeek_files/'+file, 'w')
+        f.truncate()
+        f.close()
     producer.close()
+
     # # Example to populate a Pandas dataframe from a zeek log reader
     #
     # # Collect args from the command line
